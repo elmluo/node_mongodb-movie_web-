21d5bcc31b9677b6239093f2032579207a4cb3da
qinqinluochao <349863137@qq.com>
2016-12-25 17:34:07 +0800

    [Change]1.利用mongoose数据库语言，在schema中编写数据操作方法。2.用mongoose.model将模式编译成模型，以便使用模式中定义操作数据的静态方法。

--- models/movie.js
+++ models/movie.js
@@ -1,8 +1,8 @@
 /**
  * Created by Administrator on 2016/9/13.
  */
-var mongoose = require('mongoose');
-var MovieSchema = require("../schemas/movie");
-var Movie = mongoose.model('Movie',MovieSchema);
+var mongoose = require('mongoose'); // 引入mongoose模块。
+var MovieSchema = require("../schemas/movie.js"); //引入定义好的schema模块
+var Movie = mongoose.model('Movie',MovieSchema); // 通过mongoose.model来将模式编译成模型
 
-module.exports = Movie;
+module.exports = Movie; // 导出模型
--- schemas/movie.js
+++ schemas/movie.js
@@ -1,8 +1,8 @@
 /**
  * Created by Administrator on 2016/9/13.
  */
-var mongoose = require('mongoose');
-var MovieSchema = new mongoose.Schema({
+var mongoose = require('mongoose'); //引入mongoose模块
+var MovieSchema = new mongoose.Schema({ // 创建schema对象
     doctor:String,
     title:String,
     language:String,
@@ -11,40 +11,42 @@ var MovieSchema = new mongoose.Schema({
     flash:String,
     poster:String,
     year:String,
-    meta:{
-        createAt:{
+    meta:{ // 记录的时间戳
+        createAt:{ // 创建时间
             type:Date,
             default:Date.now()
         },
-        updateAt:{
+        updateAt:{ // 跟新时间
             type:Date,
             default:Date.now()
         }
     }
 });
 
-MovieSchema.pre('save',function(next){
-    if(this.isNew){
+// 创建一个方法，pre监听，每一次存储数据之前都会调用这个方法，判断这个数据是否是新加的
+MovieSchema.pre('save',function(next){ //
+    if(this.isNew){ //判断数据是否是新加，如果新加的话，将add时间和update时间变成当前时间
         this.meta.createAt = this.meta.updateAat = Date.now()
     }
-    else{
+    else{ // 如果是跟新数据的话，将跟新时间变成当前时间
         this.meta.updateAt = Date.now()
     }
-    next()
+    next() //调用next方法才会将存储流程走下去
 });
 
+// 创建数据库查询静态方法，这些静态方法不会直接和数据库进行交互，只有通过model模型编译才会具有这个方法
 MovieSchema.statics = {
-    fetch: function (cb) {
+    fetch: function (cb) { // 封装一个查询数据库所有数据的一个方法
         return this
-            .find({})
-            .sort('meta.updateAt')
-            .exec(cb);
+            .find({}) // 查询所有
+            .sort('meta.updateAt') // 按照更新时间排序
+            .exec(cb); // 然后再执行一个回调方法
     },
-    findById: function (id, cb) {
+    findById: function (id, cb) {  // 封装一个根据id来查询单条数据的方法。
         return this
             .findOne({_id: id})
             .exec(cb);
     }
 };
 
-module.exports = MovieSchema;
+module.exports = MovieSchema; // 导出模式 然后去写模型的js代码
